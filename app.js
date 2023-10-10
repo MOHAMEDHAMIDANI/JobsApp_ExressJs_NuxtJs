8763649648f473e915d74580b164129383b3b0aa
mohamed hamidani <mohamidani1@gmail.com>
2023-10-10 16:10:19 +0200

    done with every thing except fpr the front end

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
-
+const helmet = require('helmet')
+const  rateLimit  = require('express-rate-limit')
+const xXssProtection = require("x-xss-protection");
+const cors = require('cors')
 // connect db 
 const connectDB = require('./db/connect')
 
@@ -10,14 +13,24 @@ const connectDB = require('./db/connect')
 const AuthRoute = require('./routes/auth')
 const JobsRoute = require('./routes/jobs')
 
-
-
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const authenticatedMiddleWare = require('./middleware/authentication')
-
+app.set('trust proxy' , 1)
+app.use(rateLimit({
+	windowMs: 15 * 60 * 1000, 
+	limit: 100, 
+	standardHeaders: 'draft-7', 
+	legacyHeaders: false, 
+}));
+app.use(xXssProtection());
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+
+
+
 
 // extra packages
 
